55fbe98a4a887027de95310951feb165c33abe3e
eldar <eleldar@mail.ru>
2021-03-08 01:58:53 +0300

    settings fields for admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,19 @@ from django.contrib import admin
 Django не является обязательным!)'''
 from .models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post) # обычная регистрация модели
+
+@admin.register(Post) # выполняет те же действия, что и функция admin.site.register():
+                      # регистрирует декорируемый класс – наследник ModelAdmin
+class PostAdmin(admin.ModelAdmin): # создали пользовательский класс -  наследник ModelAdmin
+    list_display = ( # атрибут list_display позволяет перечислить поля модели,
+                     # которые мы хотим отображать на странице списка
+        'title', 'slug', 'author', 'publish', 'status',
+    )
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)} # slug генерируется автоматически из поля title
+    raw_id_fields = ('author',) # поле author содержит поле поиска, что значительно упрощает
+                                # выбор автора из выпадающего списка
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
