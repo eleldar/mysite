5409f4a38f5e4de18c386adbb297da12750b7db5
eldar <eleldar@mail.ru>
2021-03-08 01:15:06 +0300

    made Post model, set admin, delete .idea

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 '''здесь мы регистрируем модели для добавления их в систему
 администрирования Django (использование сайта администрирования
 Django не является обязательным!)'''
-# Register your models here.
+from .models import Post
+
+admin.site.register(Post)
